baaf66c56673ff077befbe7eb0bfdd217a86a21b
RaimondoCosma <raimondo.cosma@gmail.com>
2022-10-24 13:33:12 +0200

    Esercizio funzionante in console.log

--- js/my-script.js
+++ js/my-script.js
@@ -1,1 +1,16 @@
 'use strict';
+// Creo un ciclo for per stampare i numeri da 1 a 100
+for (let i = 1; i <= 100; i++) {
+    // Metto Fizz sui multipli di 3
+    if (( i % 3 === 0) && !( i % 5 === 0)){
+        console.log('Fizz');
+    // Metto Buzz sui multipli di 5
+    } else if (( i % 5 === 0) && !(i % 3 === 0)){
+        console.log('Buzz');
+    // Metto FizzBuzz sui multipli sia di 3 che di 5
+    } else if (( i % 3 === 0) && (i % 5 === 0)){
+        console.log('FizzBuzz')
+    } else {
+        console.log(i)
+    }
+};
