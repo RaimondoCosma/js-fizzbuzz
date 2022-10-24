0d45da39413fa8ba466e7ae5d87aeafc318222f6
RaimondoCosma <raimondo.cosma@gmail.com>
2022-10-24 15:14:04 +0200

    Aggiunge stile completo tramite js

--- js/my-script.js
+++ js/my-script.js
@@ -1,16 +1,30 @@
 'use strict';
+const totalNumbers = document.getElementById('total-numbers');
+const box = 100;
 // Creo un ciclo for per stampare i numeri da 1 a 100
-for (let i = 1; i <= 100; i++) {
+for (let i = 1; i <= box; i++) {
+    const numberItem = document.createElement('div');
+    numberItem.classList.add('box');
     // Metto Fizz sui multipli di 3
     if (( i % 3 === 0) && !( i % 5 === 0)){
         console.log('Fizz');
+        numberItem.classList.add('box__fizz');
+        numberItem.innerHTML = 'Fizz';
     // Metto Buzz sui multipli di 5
     } else if (( i % 5 === 0) && !(i % 3 === 0)){
         console.log('Buzz');
+        numberItem.classList.add('box__buzz');
+        numberItem.innerHTML = 'Buzz';
     // Metto FizzBuzz sui multipli sia di 3 che di 5
     } else if (( i % 3 === 0) && (i % 5 === 0)){
-        console.log('FizzBuzz')
+        console.log('FizzBuzz');
+        numberItem.classList.add('box__fizz-buzz');
+        numberItem.innerHTML = 'FizzBuzz';
     } else {
         console.log(i)
+        numberItem.innerHTML = i;
     }
+    // 1. Creo variabile che prenda il container dei numeri
+    totalNumbers.append(numberItem);
 };
+
