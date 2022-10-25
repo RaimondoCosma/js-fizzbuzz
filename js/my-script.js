080e767c134a92c5ecd1aafcda7b83a1f985f92a
RaimondoCosma <raimondo.cosma@gmail.com>
2022-10-25 10:30:50 +0200

    Toglie parentesi inutili e aggiunge commenti js

--- js/my-script.js
+++ js/my-script.js
@@ -1,5 +1,7 @@
 'use strict';
+// Dichiaro una variabile e la collego al contenitore dei numeri
 const totalNumbers = document.getElementById('total-numbers');
+// Dichiaro una variabile che indica il numero di box che voglio realizzare
 const box = 100;
 // Creo un ciclo for per stampare i numeri da 1 a 100
 for (let i = 1; i <= box; i++) {
@@ -7,17 +9,17 @@ for (let i = 1; i <= box; i++) {
     numberItem.classList.add('box');
     totalNumbers.append(numberItem);
     // Metto Fizz sui multipli di 3
-    if (( i % 3 === 0) && !( i % 5 === 0)){
+    if ( i % 3 === 0 && !(i % 5 === 0) ){
         console.log('Fizz');
         numberItem.classList.add('box__fizz');
         numberItem.innerHTML = 'Fizz';
     // Metto Buzz sui multipli di 5
-    } else if (( i % 5 === 0) && !(i % 3 === 0)){
+    } else if ( i % 5 === 0 && !(i % 3 === 0) ){
         console.log('Buzz');
         numberItem.classList.add('box__buzz');
         numberItem.innerHTML = 'Buzz';
     // Metto FizzBuzz sui multipli sia di 3 che di 5
-    } else if (( i % 3 === 0) && (i % 5 === 0)){
+    } else if ( i % 3 === 0 && i % 5 === 0 ){
         console.log('FizzBuzz');
         numberItem.classList.add('box__fizz-buzz');
         numberItem.innerHTML = 'FizzBuzz';
@@ -27,3 +29,4 @@ for (let i = 1; i <= box; i++) {
     }
 };
 
+// Potevo non seguire le richieste a cascata e inserire com primo if (i % 15 === 0).
